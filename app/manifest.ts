9af12cb2a184a4a7a257e6fdc48e4f8c5fc11670
mortamc <mortamc12@gmail.com>
2026-09-19 00:08:50 -0300

    style: nueva letra y verde

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     scope: '/',
     display: 'standalone',
     background_color: '#F7F4EC',
-    theme_color: '#4D7C0F',
+    theme_color: '#0D7749',
     icons: [
       { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
       { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
